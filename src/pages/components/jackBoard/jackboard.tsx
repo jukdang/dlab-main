8838a7c1985526351f74c07895e19284a5578cf1
jukdang <jukdang479@gmail.com>
2025-06-10 18:12:50 +0900

    change structure like IGD_GAME

--- src/pages/components/jackBoard/jackboard.tsx
+++ src/pages/components/jackBoard/jackboard.tsx
@@ -1,53 +1,56 @@
 
 import { useEffect, useState } from 'react';
-import './jackboard.css'
+import JackCell from './jackcell';
+import styled from '@emotion/styled';
+
+
+const Board = styled.div`
+  padding-top: 60px;
+  display: grid;
+  grid-template-columns: repeat(9, minmax(1.5rem, 3rem));
+  gap: 0.25rem;
+  width: 100%;
+  maxWidth: 54rem;
+  margin: 0 auto;
+  @media (max-width: 768px) {
+    maxWidth: 100%;
+    width: 100%;
+    gap: 0.125rem;
+    grid-template-columns: repeat(9, 1fr);
+  };
+`;
+
 
 const shapes = ['spade', 'heart', 'diamond', 'club'];
-const nums = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
 
 const rows = 6;
 const cols = 9;
 
 const JackBoard = () => {
 
-  const [shuffledCards, setShuffledCards] = useState<string[]>([]);
-
-  const [selected, setSelected] = useState(Array(rows * cols).fill(false));
-
-  const clickCard = (index: number) => {
-    setSelected(prev => {
-      const copy = [...prev];
-      copy[index] = !copy[index]; // 토글
-      return copy;
-    });
-  };
-
-  useEffect(() => {
-    const cardNames = shapes.flatMap(shape => nums.map(num => `${shape}_${num}`));
-    cardNames.push('black_joker', 'red_joker');
-    const shuffled = [...cardNames].sort(() => Math.random() - 0.5);
-    setShuffledCards(shuffled);
-  }, []); // 최초 한 번만 실행
-
-  
-
   return (
-  <div className="table-container">
-    <div className="card-table">
-      {Array.from({ length: cols }, (_, colIndex) => (
-        <div className="card-row" key={colIndex}>
-          {shuffledCards.slice(colIndex * rows, (colIndex + 1) * rows).map((cardName, i) => (
-            <div className={`card-cell ${selected[colIndex * rows + i] ? 'selected' : ''}`} key={colIndex * rows + i} onClick={() => clickCard(colIndex * rows + i)}>
-              {cardName.includes('joker') 
-                ? (<img src='' alt={`cardName`}></img>)
-                : (<img src={`/cards/card_${cardName}.png`} alt="card" />)
-              }   
-            </div>
-          ))}
-        </div>
-      ))}
-    </div>
-  </div>
+    <Board>
+
+      {Array.from({ length: rows }).map((_, rowIdx) => 
+        Array.from({ length: cols }).map((_, colIdx) => 
+
+          <JackCell
+            isSelected={false}
+            flag={false}
+            end={false}
+            rowIdx={rowIdx}
+            colIdx={colIdx}
+            type={"number"}
+            shape={"spade"}
+            num={1}
+            onClick={() => {}}
+          />
+
+        )
+      )}
+      
+    </Board>
   )
 }
 
---
+++ src/pages/components/jackBoard/jackcell.tsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+const IMAGEPATH = '/cards/card_';
+
+
+interface CellProps {
+  isSelected: boolean;
+  flag: any;
+  end: any;
+}
+
+const Cell = styled.div<CellProps>`
+  display: flex;
+  width: 100%;
+  aspect-ratio: 1;
+  justify-content: center;
+  align-items: center;
+  flex-shrink: 0;
+  border: 1px solid #a5ada6;
+  cursor: pointer;
+  font-size: clamp(0.75rem, 3vw, 1.5rem);
+  touch-action: none;
+
+  background-color: ${({ isSelected, flag, end }) => {
+    if (isSelected) return "#E9FEE9";
+    else {
+      return flag ? "#eff0ee" : end ? "#E9FEE9" : "white";
+    }
+  }};
+
+  @media (max-width: 768px) {
+    font-size: clamp(0.5rem, 2vw, 1rem);
+    border-width: 0.5px;
+  }
+`;
+
+const CellImage = styled.img`
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+`;
+
+const jackCell = ({
+    isSelected,
+    flag,
+    end,
+    rowIdx,
+    colIdx,
+    type,
+    shape,
+    num,
+    onClick,
+}: {
+    isSelected: boolean, // 셀 선택 상태
+    flag: any, // 플래그 상태
+    end: any, // 끝 상태
+    rowIdx: number, // 행 위치
+    colIdx: number, // 열 위치
+    type: String, // 카드 타입
+    shape: String, // 카드 모양
+    num: number, // 카드 숫자
+    onClick: () => void, // 클릭 이벤트 핸들러
+}) => {
+
+
+    return (
+        <Cell
+            id={`${rowIdx}_${colIdx}`}
+            isSelected={isSelected}
+            flag={flag}
+            end={end} 
+            onClick={onClick}
+        >
+            {type === 'number' 
+                ? <CellImage src={`${IMAGEPATH}${shape}_${num}.png`} alt="card" />
+                : <CellImage src='' alt={`cardName`} />
+            }
+            
+        </Cell>
+    );
+};
+
+export default jackCell;
